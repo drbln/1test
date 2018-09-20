07873dbea80e1d8bf3acb0d8b91faba7ee322fd4
drbln <r_oleg94@mail.ru>
2018-09-20 09:14:54 +0300

    full reg

--- Mysite.rb
+++ Mysite.rb
@@ -3,3 +3,38 @@ require 'sinatra'
 get '/' do
 	erb :index
 end
+
+get '/reg' do
+	erb :reg
+end
+
+post '/' do
+	@user_name = params[:user_name]
+	@password = params[:password]
+	File.open 'logpass.txt', 'r' do |file|
+		if file.read().include? "#{@user_name},#{@password}"
+			erb :welcome
+			else @massage = "Incorrect login or password" 
+				erb :index
+		end
+	end
+end
+
+post '/reg' do
+	@user_name = params[:user_name]
+	@password = params[:password]
+		File.open 'logpass.txt', 'r+' do |file|
+			if file.read().include? "#{@user_name}"
+				@massage2 = "Login is busy"
+				erb :reg
+				else logpass = File.open 'logpass.txt', 'a'
+					 logpass.write "#{@user_name},#{@password}\n"
+					 logpass.close
+					 erb :index
+			end
+		end
+	#logpass = File.open 'logpass.txt', 'a'
+	#logpass.write "#{@user_name},#{@password}\n"
+	#logpass.close
+	#erb :index
+end
